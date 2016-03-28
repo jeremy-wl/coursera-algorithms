02cccdfc0502db9bc040f9406bd0f7bb788ce07d
Jeremy Wong <jwong8593@gmail.com>
2016-03-29 00:28:30 +0800

    Red-black BST basic operations

---
+++ Week 5_1 Balanced Search Trees/RedBlackBST.java
@@ -0,0 +1,100 @@
+/**
+ * Created by Jeremy on 3/27/16.
+ */
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;     // root of the BST
+
+    private class Node {
+        private Key key;           // key
+        private Value value;         // associated data
+        private Node left, right;  // links to left and right subtrees
+        private boolean color;     // color of parent link
+
+        public Node(Key key, Value value, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+        }
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0)
+                x = x.left;
+            else if (cmp > 0)
+                x = x.right;
+            else
+                return x.value;
+        }
+        return null;
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null)
+            return false;
+        return x.color == RED;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    public void put(Key key, Value value) {
+        if (value == null) {
+            return;
+        }
+        root = put(root, key, value);
+    }
+
+    private Node put(Node h, Key key, Value value) {
+        if (h == null)
+            return new Node(key, value, RED);
+
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0)
+            h.left = put(h.left, key, value);
+        else if (cmp > 0)
+            h.right = put(h.right, key, value);
+        else
+            h.value = value;
+
+        if (isRed(h.right) && !isRed(h.left))
+            h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left))
+            h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right))
+            flipColors(h);
+
+        return h;
+    }
+}
